6172912c19e7063c98025d5e8652bc562d38aa5a
Jessy SCHNEIDER <schneider.jessy54@gmail.com>
2018-10-29 15:00:36 +0100

    Structure login

--- MagieMagie/src/app/app.module.ts
+++ MagieMagie/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { AppComponent } from './app.component';
-import { BoutonInterfaceRedComponent } from './element-login/bouton-interface-red/bouton-interface-red.component';
+import { AppComponent } from './app.component';;
 import { ListeAvatarComponent } from './element-login/liste-avatar/liste-avatar.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './element-login/header/header.component';
@@ -11,7 +10,6 @@ import { HeaderComponent } from './element-login/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BoutonInterfaceRedComponent,
     ListeAvatarComponent,
     LoginComponent,
     HeaderComponent
