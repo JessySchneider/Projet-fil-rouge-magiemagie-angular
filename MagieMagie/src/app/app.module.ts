8eb8499fd814f38ce4b1164dd880eee787ea43b4
Jessy SCHNEIDER <schneider.jessy54@gmail.com>
2018-10-29 12:35:56 +0100

    Installation de bootstrap

--- MagieMagie/src/app/app.module.ts
+++ MagieMagie/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { BoutonInterfaceRedComponent } from './bouton-interface-red/bouton-interface-red.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoutonInterfaceRedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MagieMagie/src/app/bouton-interface-red/bouton-interface-red.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bouton-interface-red',
+  templateUrl: './bouton-interface-red.component.html',
+  styleUrls: ['./bouton-interface-red.component.scss']
+})
+export class BoutonInterfaceRedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
