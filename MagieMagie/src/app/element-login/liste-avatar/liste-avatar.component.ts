edab0153079104d19a57c27ab4c98e9b6c34b72d
seb <spoussard@outlook.fr>
2018-10-29 16:54:48 +0100

    page login a 50% manque les codes d'interaction

--- MagieMagie/src/app/element-login/liste-avatar/liste-avatar.component.ts
+++ MagieMagie/src/app/element-login/liste-avatar/liste-avatar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+class Product {
+}
+
 @Component({
   selector: 'app-liste-avatar',
   templateUrl: './liste-avatar.component.html',
   styleUrls: ['./liste-avatar.component.scss']
 })
 export class ListeAvatarComponent implements OnInit {
+  products: Product[];
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+
   }
 
 }
